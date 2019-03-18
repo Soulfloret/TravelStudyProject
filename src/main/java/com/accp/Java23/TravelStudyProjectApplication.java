37233f59f4aab30aeb4be24d380567da7bceaf0a
Soulfloret <1931369335@qq.com>
2019-03-18 11:19:15 +0800

    # WARNING: head commit changed in the meantime
    项目创建

---
+++ src/main/java/com/accp/Java23/TravelStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.accp.Java23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelStudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelStudyProjectApplication.class, args);
+	}
+
+}
