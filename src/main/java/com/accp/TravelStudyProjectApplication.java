d6f2a4e105b0109501a6d13252a245e9ce12b38d
Soulfloret <1931369335@qq.com>
2019-04-08 11:47:39 +0800

    项目搭建

--- src/main/java/com/accp/TravelStudyProjectApplication.java
+++ src/main/java/com/accp/TravelStudyProjectApplication.java
@@ -1,9 +1,11 @@
 package com.accp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@MapperScan("com.accp.mapper")
 public class TravelStudyProjectApplication {
 
 	public static void main(String[] args) {
