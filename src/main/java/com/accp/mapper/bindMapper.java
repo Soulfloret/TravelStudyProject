9013633eb419031d0f47f38aeca1868ab1c6270d
RengYuXuan <1720404911@qq.com>
2019-04-09 16:46:55 +0800

    ejjeje

--- src/main/java/com/accp/mapper/bindMapper.java
+++ src/main/java/com/accp/mapper/bindMapper.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface bindMapper {
+	
+	List<bind> querybind(bind bi);
+	
     int countByExample(bindExample example);
 
     int deleteByExample(bindExample example);
