6034e087dc7c4e29767c530e7cb2cc61529dda8c
jiajulolo <1771602877@qq.com>
2019-04-10 15:50:54 +0800

    员工模糊查询

--- src/main/java/com/accp/wujiajun/controller/staffController.java
+++ src/main/java/com/accp/wujiajun/controller/staffController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.accp.domain.positions;
 import com.accp.domain.staff;
+import com.accp.wujiajun.service.positionsService;
 import com.accp.wujiajun.service.staffService;
 
 @Controller
@@ -17,21 +20,33 @@ public class staffController {
 	@Autowired
 	staffService service;
 	
+	@Autowired
+	positionsService positions;
+	
+	@RequestMapping("/queryLike")
+	public String queryLike(staff sta) {
+		List<staff> list=service.queryLike(sta);
+		
+		return "JJ_customer";
+	}
+	
 	@RequestMapping("/query")
 	public String query(staff sta,Model model) {
 		List<staff> list=service.query(sta);
-		System.out.println(list);
-		for (staff staff : list) {
-			System.out.println(staff.getUlist().getUname());
-		}
+		List<positions> lists=positions.selectByExample(null);
 		model.addAttribute("list", list);
+		model.addAttribute("lists",lists);
+		for (positions positions : lists) {
+			System.out.println(positions.getPostion());
+		}
 		return "JJ_customer";
 	}
 	
 	@RequestMapping("/queryById")
-	public String queryById(Integer id,Model model) {
+	@ResponseBody
+	public staff queryById(Integer id,Model model) {
 		staff lists=service.queryById(id);
-		model.addAttribute(lists);
-		return "JJ_customer";
+		/*model.addAttribute(lists);*/
+		return lists;
 	}
 }
