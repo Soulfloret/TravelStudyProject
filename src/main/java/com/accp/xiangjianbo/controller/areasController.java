0457bfa4c26735ef7f74dad1937759a230b15c61
xiaobo5869 <2373111030@qq.com>
2019-04-16 10:49:03 +0800

    # WARNING: head commit changed in the meantime
    Merge remote-tracking branch 'origin/xiangjianbo' into xiangjianbo1

--- src/main/java/com/accp/xiangjianbo/controller/areasController.java
+++ src/main/java/com/accp/xiangjianbo/controller/areasController.java
@@ -1,12 +1,16 @@
 package com.accp.xiangjianbo.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.accp.domain.areas;
 import com.accp.domain.areasExample;
@@ -36,6 +40,7 @@ public class areasController {
 	
 	@RequestMapping("/insert")
 	public String insert(areas area) {
+		
 		int i=areas.insert(area);
 		return "redirect:query";
 	}
