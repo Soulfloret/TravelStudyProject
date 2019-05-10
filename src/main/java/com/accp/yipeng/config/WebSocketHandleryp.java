2729964b5d04630c348c5be3b46f78eedcceae9f
yp2002 <peng520yp@163.com>
2019-05-10 12:08:14 +0800

    5.10yip

--- src/main/java/com/accp/yipeng/config/WebSocketHandleryp.java
+++ src/main/java/com/accp/yipeng/config/WebSocketHandleryp.java
@@ -24,16 +24,24 @@ public class WebSocketHandleryp extends TextWebSocketHandler {
 	//建立连接后
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		users use=(users)session.getAttributes().get("use");
+		map.put(use.getId()+"", session);
 		List<discussiongroup> list =use.getDlist();
 		for (discussiongroup discussiongroup : list) {
 			for (discussiongroupson discussiongroupson : discussiongroup.getDlist()) {
 				if(discussiongroupson.getUserid()==use.getId()) {
-					if(MAP1.get(discussiongroup.getId().toString())!=null) {
+					//System.out.println(MAP1.get(discussiongroup.getId()+""));
+					if(MAP1.get(discussiongroup.getId()+"")!=null) {
 						List<WebSocketSession> list1=MAP1.get(discussiongroup.getId()+"");
-						list1.add(session);
+						for (WebSocketSession wss : list1) {
+							Map<String, Object> ss=wss.getAttributes();
+							users uses=(users)ss.get("use");
+							if(uses.getId()!=use.getId()) {
+								list1.add(map.get(use.getId()+""));
+							}
+						}
 					}else {
 						List<WebSocketSession> list1=new ArrayList<WebSocketSession>();
-						list1.add(session);
+						list1.add(map.get(use.getId()+""));
 						MAP1.put(discussiongroup.getId()+"",list1);
 					}
 					
@@ -42,11 +50,16 @@ public class WebSocketHandleryp extends TextWebSocketHandler {
 		}
 		session.sendMessage(new TextMessage("建立连接成功，可以进行通信"));
 	}
-	public void sendMsg(String message,String  did) {
+	public void sendMsg(String message,String  did,String uid) {
 			List<WebSocketSession> ws=MAP1.get(did);
 			try {
 				for (WebSocketSession wss : ws) {
-					wss.sendMessage(new TextMessage(message));
+					Map<String, Object> ss=wss.getAttributes();
+					users use=(users)ss.get("use");
+					if(use.getId()!=Integer.parseInt(uid)) {
+						wss.sendMessage(new TextMessage(message));
+					}
+					
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
